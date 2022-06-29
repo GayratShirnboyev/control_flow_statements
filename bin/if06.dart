c820184405b0860c8371a4ca842cd2c68f3533ec
G'ayrat <shirnboyevgayrat@gmail.com>
2022-06-29 10:39:10 +0500

    data6

--- bin/if06.dart
+++ bin/if06.dart
@@ -3,7 +3,7 @@
     Create a function argument  called a,b,c of type int
     Find how many positive and how many negative numbers there are in the given arguments.
     check the following conditions:
-    "there are a lot of positive numbers",
+    "",
     "there are a lot of negative numbers"
     Args:
         a: first number
@@ -11,4 +11,20 @@
         c: third number
     Returns:
         string: string with the result*/
-void main() {}
+int func(a, b, c) {
+  int x = 0;
+  if (a < 0) {
+    x = x + 1;
+  }
+  if (b < 0) {
+    x = x + 1;
+  }
+  if (c < 0) {
+    x = x + 1;
+  }
+  return x;
+}
+
+void main() {
+  print(func(-2, -4, -6));
+}
