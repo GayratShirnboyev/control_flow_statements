033c11750d402bfabf5b9460cf71c82a5a0a6cb1
G'ayrat <shirnboyevgayrat@gmail.com>
2022-06-29 13:18:06 +0500

    data7

--- bin/if07.dart
+++ bin/if07.dart
@@ -11,4 +11,37 @@
         a: integer
     Returns:
         string: the message to print */
-void main() {}
+String func(number) {
+  String t = "positive odd number";
+  String l = "positive edd number";
+  String b = "negative even number";
+  String f = "the number even number";
+  String m = "negative o is zero";
+
+  if (number > 0) {
+    if ((number % 2) == 1) {}
+    return t;
+  }
+  if (number > 0) {
+    if ((number % 2) == 0) {}
+    return l;
+  }
+
+  if (number > 0) {
+    if ((number % 2) == 1) {}
+    return b;
+  }
+  if (number > 0) {
+    if ((number % 2) == 1) {}
+    return f;
+  }
+
+  if (number > 0) {
+    if ((number % 2) == 1) {}
+    return m;
+  }
+}
+
+void main() {
+  print(func(678));
+}
